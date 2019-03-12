64ca845203f055da642a0d0e35abfb0fbbd12ce1
FlameTinary <123828091@qq.com>
2019-03-12 13:14:21 +0800

    测试modified

--- TYToolSet/Examples/Controllers/TYBaseViewController.m
+++ TYToolSet/Examples/Controllers/TYBaseViewController.m
@@ -18,6 +18,7 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view.
     self.view.backgroundColor = [UIColor whiteColor];
+    NSLog(@"无用文字");
 }
 
 - (void)didReceiveMemoryWarning {
